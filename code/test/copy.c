4ead2ab1e00a7a266d06f0e6cc84cd6ddce7279d
toanle312 <toanle3122002@gmail.com>
2022-11-12 15:06:41 +0000

    Complete user program copy and createfile

--- code/test/copy.c
+++ code/test/copy.c
@@ -5,16 +5,67 @@
 int
 main(int argc, char *argv[])
 {
-    PrintNum(argc);
+    char src[255];
+    char dest[255];
+    char srcBuf[255];
 
-    // if(argc != 5)
-    // {
-    //     PrintString("Wrong parmeter");
-    // }
-    // else{
-    //     PrintString(source);
-    //     PrintString(destination);
-    // }
+    int srcId;
+    int destId;
+
+    int i;
+    int isValidFileName = 1;
+    int temp;
+
+    PrintString("Input name of source file that you want to copy: ");
+    ReadString(src, 255);
+
+    PrintString("Input name of destination file that you want to paste: ");
+    ReadString(dest, 255);
+
+    srcId = Open(src);
+
+    if(srcId != -1)
+    {
+        Read(srcBuf,255,srcId);
+
+        if(!dest[0])
+        {
+            isValidFileName = 0;
+        }
+
+        if(isValidFileName != 0)
+        {
+            if(Create(dest) == 0)
+            {
+                destId = Open(dest);
+
+                if(destId != -1)
+                {
+                    Write(srcBuf,255,destId);
+
+                    PrintString("Copy succeed !!!\n");
+
+                    Close(destId);
+                }
+                else{
+                    PrintString("Can not open destination file !!!\n");
+                }
+            }
+            else{
+                PrintString("Can not create destination file !!!\n");
+            }
+        }
+        else{
+            PrintString("Unvalid destination file name!!!\n");
+        }
+    }
+    else{
+        PrintString("Can not open source file !!!\n");
+    }
+
+    Close(srcId);
+
+    Halt();
 
     return 0;
 }
--- code/test/createfile.c
+++ code/test/createfile.c
@@ -2,14 +2,35 @@
 
 int main()
 {
-    if (Create("test1.txt") == -1)
+    char fileName[255];
+    int isValidFileName = 1;
+    int i = 0;
+
+    PrintString("Input name of file that you want to create: ");
+    ReadString(fileName, 255);
+
+    if(!fileName[0])
     {
-      PrintString("Can not create file test.txt\n");
-    } else
-    {
-      PrintString("Create file test.txt successfull\n");
+      isValidFileName = 0;
     }
-      
+  
+    if(isValidFileName == 0)
+    {
+      PrintString("Unvalid file name !!!\n");
+    }
+    else{
+      if(Create(fileName) == 0)
+      {
+        PrintString("Create file ");
+        PrintString(fileName);
+        PrintString(" succeed !!!\n");
+      }
+      else{
+        PrintString("Can not create file ");
+        PrintString(fileName);
+        PrintString("\n");
+      }
+    }
     Halt();
     return 0;
 }
