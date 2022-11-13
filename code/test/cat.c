0d91ffc756d2957d0c6c8fc196b52c98804fe9dc
N.A.Tuan <20120395@student.hcmus.edu.vn>
2022-11-13 17:00:12 +0000

    Merge branch 'master' of https://github.com/toanle312/NachOS-File-Handling

--- code/test/cat.c
+++ code/test/cat.c
@@ -7,18 +7,25 @@ int main() {
     OpenFileId fileId;
     char fileName[MAX_STRING_LENGTH];
 
+    // Input file name
     PrintString("Nhap ten file: ");
     ReadString(fileName,MAX_STRING_LENGTH);
 
     fileId = Open(fileName);
+
+    // file is open
     if (fileId != -1) {
         int run = 1;
         char content[MAX_STRING_LENGTH];
         int numRead = 0;
         do {
+            // Read content from file
             numRead = Read(content,MAX_STRING_LENGTH,fileId);
+
+            // Print content to console
             PrintString(content);
 
+            // check if reach end of the file
             if (numRead < MAX_STRING_LENGTH) {
                 run = 0;
             }
---
+++ code/test/concatenate.c
@@ -0,0 +1,59 @@
+
+#include "syscall.h"
+#define MAX_STRING_LENGTH 255
+
+/*
+Nguyễn Anh Tuấn - 20120395
+Input:
+	@fileName1  source file 1
+    @fileName2  source file 2
+Output:
+    modified file 1
+Purpose:
+    Concatenate content from file 2 to the last position of file 1
+*/
+int main() {
+
+    OpenFileId fileId1;
+    char fileName1[MAX_STRING_LENGTH];
+
+    OpenFileId fileId2;
+    char fileName2[MAX_STRING_LENGTH];
+
+    // Input file name
+    PrintString("Nhap ten file nguon 1: ");
+    ReadString(fileName1,MAX_STRING_LENGTH);
+
+    PrintString("Nhap ten file nguon 2: ");
+    ReadString(fileName2,MAX_STRING_LENGTH);
+
+    fileId1 = Open(fileName1);
+    fileId2 = Open(fileName2);
+
+    // both files is open
+    if (fileId1 != -1 && fileId2 != -1) {
+        int run = 1;
+        char content[MAX_STRING_LENGTH];
+        int numRead = 0;
+
+        Seek(-1, fileId1);
+        do {
+            // Read content from file 2
+            numRead = Read(content,MAX_STRING_LENGTH,fileId2);
+
+            // Write content from file2 to end of file1
+            Write(content,fileId1);
+
+            // check if reach end of the file
+            if (numRead < MAX_STRING_LENGTH) {
+                run = 0;
+            }
+        }while(run);
+    } else {
+        PrintString("[Error] file is not exist\n");
+    }
+    Close(fileId1);
+    Close(fileId2);
+
+    Halt();
+}
